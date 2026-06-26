34f40be29452444e61ec9a991562c03a37d8eae6
Diana Monteiro <dianar.monteiro@gmail.com>
2026-06-26 16:59:53 -0300

    add: projeto buildado e arquivos .jar

--- target/generated-sources/annotations/com/plataforma/conversacional/mapper/DocumentMapperImpl.java
+++ target/generated-sources/annotations/com/plataforma/conversacional/mapper/DocumentMapperImpl.java
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-06-26T16:07:44-0300",
-    comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.46.100.v20260624-0231, environment: Java 21.0.11 (Eclipse Adoptium)"
+    date = "2026-06-26T16:55:51-0300",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.12 (Oracle Corporation)"
 )
 @Component
 public class DocumentMapperImpl implements DocumentMapper {
